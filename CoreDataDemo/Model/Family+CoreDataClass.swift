50b8e43d734728ce50d6a884706d0b5b39a503b9
jessicaguiot <jessica.guiot97@gmail.com>
2020-10-14 11:07:25 -0300

    implementing CRUD with CoreData

---
+++ CoreDataDemo/Model/Family+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Family+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Jéssica Araujo on 14/10/20.
+//  Copyright © 2020 academy. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Family)
+public class Family: NSManagedObject {
+
+}
---
+++ CoreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Jéssica Araujo on 14/10/20.
+//  Copyright © 2020 academy. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
