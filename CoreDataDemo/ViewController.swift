f42bdc6497dac05c2dad7daf205da9016df5180e
jessicaguiot <jessica.guiot97@gmail.com>
2020-10-14 09:21:29 -0300

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Jéssica Araujo on 14/10/20.
+//  Copyright © 2020 academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
